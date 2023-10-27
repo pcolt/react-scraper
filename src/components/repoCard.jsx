93bd9c700a17f3a4b01ffe13188aafd229b29b8d
Pablo Colturi <pablo.colturi@fashiontamers.com>
2023-10-27 11:13:56 +0200

    add github logo

--- src/components/repoCard.jsx
+++ src/components/repoCard.jsx
@@ -1,11 +1,12 @@
 import './repoCard.css'
+import githublogo from '../assets/github-mark.svg'
 export const RepoCard = ({ repo }) => {
 
     return (
       <div className="repoCard">
         <h2>{repo.repoName}</h2>
         <p>{repo.description}</p>
-        <a href="{repo.url}">GitHub</a>
+        <a href="{repo.url}"><img className="logo" src={githublogo} />GitHub</a>
         <p>Stars &#11088; {repo.stars}</p>
       </div>
     )
