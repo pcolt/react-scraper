d893c27fd6c46b5af2b41bbe5b716a699cdffe8b
Pablo Colturi <pablo.colturi@fashiontamers.com>
2023-11-17 13:07:29 +0100

    refactor Repos in its own component

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,13 @@
-import { useEffect, useState, useRef } from 'react'
-import axios from 'axios'
-import { RepoCard } from './components/repoCard'
+import { useEffect, useState } from 'react'
+// import axios from 'axios'
 import { LoginForm } from './components/loginForm'
 import Togglable from './components/togglable'
-import {compareByName, compareByStars, topics } from './services/helpers'
+import Repos from './components/repos'
 import { UpdateRepos } from './components/updateRepos'
 
-const baseUrl = '/api/repos'
-
 const App = () => {
-  const [orderType, setOrderType] = useState(null)
-  const [repos, setRepos] = useState([])
-  const [sortedRepos, setSortedRepos] = useState([]) // new state variable
-  const [repoSelected, setRepoSelected] = useState('')
   const [user, setUser] = useState(null)
   const [token, setToken] = useState(null)
-
-
-  const handleClickOrderByName = () => {
-    setOrderType('name')
-  }
-
-  const handleClickOrderByStars = () => {
-    setOrderType('stars')
-  }
-
-  const handleSelectRepo = event => {
-    console.log(event.target.value)
-    setRepoSelected(event.target.value)
-  }
 
   const handleUserChange = (newUser) => {
     setUser(newUser)
@@ -53,61 +32,10 @@ const App = () => {
     }  
   }, [])
 
-  useEffect(() => {   // whenever orderType changes 
-    if (orderType === null) { return }
-
-    console.log(`Reorder by ${orderType}`)
-    let copyRepos = repos.slice()
-
-    if (orderType === 'name') {
-      setSortedRepos(copyRepos.sort(compareByName))
-    } 
-    if (orderType === 'stars') {
-      setSortedRepos(copyRepos.sort(compareByStars))
-    }
-  }, [orderType, repos])
-
-  useEffect(() => {   // whenever repoSelected changes 
-    if (repoSelected === '') { return }
-
-    console.log(`Display ${repoSelected}`)
-    setOrderType(null)
-    setSortedRepos([])
-
-    axios
-      .get(`${baseUrl}/${repoSelected}`).then(response => {
-        console.log(`repos ${repoSelected} retrieved`)
-        setRepos(response.data)
-      }) 
-
-  }, [repoSelected])
-
   return (
     <div>
-      <h1>Repos</h1>
-      <div>
-        <h3>Select a topic to search:</h3>
-        <select value={repoSelected} onChange={handleSelectRepo}>
-          <option value='' disabled={true}>Select an option</option>
-          {topics.map(topic => (
-            <option key={topic.value} value={topic.value}>{topic.display}</option>
-          ))}
-        </select>
-      </div>
-      <div>
-        <h3>Order by</h3>
-        <button className={(orderType === 'name')? 'button-selected' : ''} onClick={handleClickOrderByName}>
-          {(orderType === 'name') ? 'ordered' : 'order'} by Name
-        </button>
-        <button className={(orderType === 'stars')? 'button-selected' : ''} onClick={handleClickOrderByStars}>
-          {(orderType === 'stars') ? 'ordered' : 'order'} by Stars
-        </button>
-      </div>
-      <div>
-        {(sortedRepos.length > 0 ? sortedRepos : repos).map(repo =>
-          <RepoCard key={repo.id} repo={repo} />
-        )}
-      </div> 
+
+      <Repos></Repos>
 
       {
         user === null 
---
+++ src/components/repos.jsx
@@ -0,0 +1,87 @@
+import {compareByName, compareByStars, topics } from '../services/helpers'
+import { useEffect, useState } from 'react'
+import { RepoCard } from './repoCard'
+import axios from 'axios'
+
+
+const Repos = () => {
+  const [orderType, setOrderType] = useState(null)
+  const [repos, setRepos] = useState([])
+  const [sortedRepos, setSortedRepos] = useState([]) // new state variable
+  const [repoSelected, setRepoSelected] = useState('')
+
+  const baseUrl = '/api/repos'
+
+  const handleClickOrderByName = () => {
+    setOrderType('name')
+  }
+
+  const handleClickOrderByStars = () => {
+    setOrderType('stars')
+  }
+
+  const handleSelectRepo = event => {
+    console.log(event.target.value)
+    setRepoSelected(event.target.value)
+  }
+
+  useEffect(() => {   // whenever orderType changes 
+    if (orderType === null) { return }
+
+    console.log(`Reorder by ${orderType}`)
+    let copyRepos = repos.slice()
+
+    if (orderType === 'name') {
+      setSortedRepos(copyRepos.sort(compareByName))
+    } 
+    if (orderType === 'stars') {
+      setSortedRepos(copyRepos.sort(compareByStars))
+    }
+  }, [orderType, repos])
+
+  useEffect(() => {   // whenever repoSelected changes 
+    if (repoSelected === '') { return }
+
+    console.log(`Display ${repoSelected}`)
+    setOrderType(null)
+    setSortedRepos([])
+
+    axios
+      .get(`${baseUrl}/${repoSelected}`).then(response => {
+        console.log(`repos ${repoSelected} retrieved`)
+        setRepos(response.data)
+      }) 
+
+  }, [repoSelected])
+
+  return (
+    <div>
+      <h1>Repos</h1>
+      <div>
+        <h3>Select a topic to search:</h3>
+        <select value={repoSelected} onChange={handleSelectRepo}>
+          <option value='' disabled={true}>Select an option</option>
+          {topics.map(topic => (
+            <option key={topic.value} value={topic.value}>{topic.display}</option>
+          ))}
+        </select>
+      </div>
+      <div>
+        <h3>Order by</h3>
+        <button className={(orderType === 'name')? 'button-selected' : ''} onClick={handleClickOrderByName}>
+          {(orderType === 'name') ? 'ordered' : 'order'} by Name
+        </button>
+        <button className={(orderType === 'stars')? 'button-selected' : ''} onClick={handleClickOrderByStars}>
+          {(orderType === 'stars') ? 'ordered' : 'order'} by Stars
+        </button>
+      </div>
+      <div>
+        {(sortedRepos.length > 0 ? sortedRepos : repos).map(repo =>
+          <RepoCard key={repo.id} repo={repo} />
+        )}
+      </div> 
+    </div>
+  )
+}
+
+export default Repos
