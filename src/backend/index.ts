239817f0e0be0e8256193e937e4db981372c146f
pcolt <pcolturi@proton.me>
2024-02-14 16:51:28 +0100

    FIX start backend dev command

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -1,7 +1,7 @@
 // import app from './app.js'
-import config from './utils/config.js'
-import logger from './utils/logger.js'
-import app from './app.js'
+import config from './utils/config'
+import logger from './utils/logger'
+import app from './app'
 // const config = require('./utils/config.js')
 // const logger = require('./utils/logger.js')
 
